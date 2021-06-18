e7bb5bb77097b46ca73b74315d0c67d67e571f7f
yooooyo <loipswqe@gmail.com>
2021-06-18 12:00:40 +0800

    add python tutorial on README, quiz and test for quiz

---
+++ tests/test_lesson1.py
@@ -0,0 +1,11 @@
+from lesson1.quiz import *
+
+def test_list_quiz():
+    assert list_quiz() == (500+0.25/10)
+
+def test_dict_quiz():
+    assert dict_quiz() == {}
+
+def test_search_cpu_codename():
+    assert search_cpu_codename([2015,2016,2021]) == ['kaby lake','ice lake',None]
+    
