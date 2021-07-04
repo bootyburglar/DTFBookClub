b1a4bdae76de2edaac2eebe1c9d007fc901e74e0
Frank Lu <lu.phrank@gmail.com>
2021-07-04 17:11:14 +0800

    modified quotes to use double.

--- lesson2/quiz.py
+++ lesson2/quiz.py
@@ -1,9 +1,4 @@
 from typing import List, Dict, AnyStr
-"""
-2.1 even
-input: a list of numbers
-output: return only the even numbers
-"""
 
 
 def even(numbers: List) -> List:
@@ -22,17 +17,8 @@ def even(numbers: List) -> List:
     pass
 
 
-"""
-2.2 fizzbuzz
-input: a list of numbers
-output: print out fizz if divisible by 3
-        print out fuzz if divisible by 5
-        print out fizzbuzz if divisible by both 3 and 5
-"""
-
-
 def fizzbuzz(numbers: List) -> List:
-    '''
+    """
     輸入一串數字numbers
     3的倍數輸出fizz
     5的倍數輸出buzz
@@ -48,21 +34,14 @@ def fizzbuzz(numbers: List) -> List:
     Example:
         fizzbuzz([1,2,3,4,5,15])
         >[1,2,'fizz',4,'buzz','fizzbuzz']
-    '''
+    """
     result = None
     #code here
     return result
 
 
-"""
-2.3 vending machine
-input: an insert value and a product value
-output: a dictionary of change and its numbers
-"""
-
-
 def vending_machine(product: int, insert: int) -> Dict:
-    '''
+    """
     輸入product代表購買總價格 insert代表投入的錢 輸出找的錢 以dict來表示
 
     Args:
@@ -80,17 +59,10 @@ def vending_machine(product: int, insert: int) -> Dict:
             10:1
             5:1
         }
-    '''
+    """
     result = {}
     #code here
     return result
-
-
-"""
-2.4 convert
-input: a value n and a base
-output: the value in its specific base
-"""
 
 
 def convert(num: int, base: int) -> str:
@@ -113,15 +85,8 @@ def convert(num: int, base: int) -> str:
     return result
 
 
-"""
-2.5.1 is_prime
-input: a number
-output: True if the number is prime, False if not
-"""
-
-
 def is_prime(num: int) -> bool:
-    '''
+    """
     找出質數 是質數return True否則為False
 
     Args:
@@ -129,20 +94,13 @@ def is_prime(num: int) -> bool:
 
     Returns:
         boolean
-    '''
+    """
     ## code here
     return
 
 
-"""
-2.5.2 eratosthenes
-input: a number n
-output: a list of prime numbers smaller than n
-"""
-
-
 def eratosthenes(num: int) -> List:
-    '''
+    """
     找出num範圍內的質數
 
     Args:
@@ -154,20 +112,13 @@ def eratosthenes(num: int) -> List:
     Example:
         eratosthenes(10)
         > [1,2,3,5,7]
-    '''
+    """
     #code here
     pass
 
 
-"""
-2.6 fibonacci
-input: a number n
-output: a list of fibonacci numbers smaller than n
-"""
-
-
 def fibonacci(num: int) -> int:
-    '''
+    """
     fibonacci 數列 前兩個數總和等於第三個
     1,1,2,3,5,8,13,21......
     數入num代表要輸出fibonacci數列裡面第幾個數字
@@ -183,41 +134,27 @@ def fibonacci(num: int) -> int:
         > 5
         fibonacci(8)
         > 21
-    '''
+    """
     fibonacci_init_list = [1, 1]
     #code here
     return
 
 
-"""
-quiz 1 leap year from 1950 to 2050
-input: None
-output: a leap year list
-"""
-
-
 def leap_year() -> List:
-    '''
+    """
     找出1950到2050間的閏年
     能被4乘除的年份是閏年
     可以被100整除但不能被400整除的不是閏年
 
     Returns:
         list
-    '''
+    """
     #code here
     return
 
 
-"""
-quiz 2 convert year to japan year
-input: 1986 <= year <= 2020
-output: japan year name
-"""
-
-
 def japan_year(year: int) -> str:
-    '''
+    """
     輸入year西元年份 輸出日本年份
     明治元年1868 大正元年1912 昭和元年1926 平成元年1989 令和元年2019
 
@@ -230,6 +167,6 @@ def japan_year(year: int) -> str:
     Examples:
         japan_year(2000))
         > 平成12年
-    '''
+    """
     #code here
     return
